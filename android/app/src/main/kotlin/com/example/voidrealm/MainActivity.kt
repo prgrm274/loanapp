032270315d2a9d40a5a9a141e06c717eb302e868
prgrm274 <programmer270487@gmail.com>
2020-10-18 20:26:40 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/voidrealm/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.voidrealm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    new Center(
+      child: new MyApp(),
+    )
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+
+    );
+  }
+}
