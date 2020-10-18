55555b538e21cbfca3b58074220f1d5e29946e67
prgrm274 <programmer270487@gmail.com>
2020-10-18 20:27:09 +0700

    Scaffold tok

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Flutter Demo',
-
+      home: new Scaffold(),
     );
   }
 }
