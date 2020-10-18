4dd1ac756b182aa71bcd923db424565952c27844
prgrm274 <programmer270487@gmail.com>
2020-10-18 22:26:25 +0700

    23 StatefulWidget dan State nya, dan AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:voidrealm/helloworld.dart';
 
 void main() => runApp(new MyApp());
 
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {///WONT CHANGE
+  @override
+  _Statenya createState() => new _Statenya();
+}
+
+/**
+ * This is the state of MyApp
+ * where you can put widgets, anything else..
+ */
+
+///_ IS PRIVATE
+class _Statenya extends State<MyApp> {///WILL CHANGE
+  String _title = 'App Bar demo';
+  String _myState = '';
+
+  void _pressed(String message) {
+    print(message);
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: '21 Basic Layout',
+      title: _title,
+
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text('Basic Layout'),
+          title: new Text(_title),
+          actions: <Widget>[
+            new IconButton(
+                icon: new Icon(Icons.add_alert),
+                onPressed: (){
+                  _pressed('Alert pressed');
+                })
+          ],
         ),
         body: new Container(
           padding: const EdgeInsets.all(32),
-
           child: new Center(
-            child: new Column(
-              children: <Widget>[
-                new Text('array text 0'),
-                new Text('array text 1'),
-                new Text('array text 2'),
-                new Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    /*new Container(
-                      padding: const EdgeInsets.all(5),
-                      child: new Column(
-                        children: <Widget>[
-                          new Text('array text 0'),
-                          new Text('array text 1'),
-                          new Text('array text 2'),
-                        ],
-                      ),
-                    ),*/
-                    new hello(),
-                    new hello(),
-                    new hello(),
-                  ],
-                )
-              ],
-            ),
+            child: new Text('Hai'),
           ),
         ),
       ),
     );
   }
+
 }
