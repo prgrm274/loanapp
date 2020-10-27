ff721d9005a2b69c0ef34bcb8e35d50233249471
prgrm274 <programmer270487@gmail.com>
2020-10-27 10:24:52 +0700

    permission_request

--- lib/apps/permissions/permission_request.dart
+++ lib/apps/permissions/permission_request.dart
@@ -22,89 +22,170 @@ class _PermissionRequestState extends State<PermissionRequest> {
         scrollDirection: Axis.vertical,
         shrinkWrap: true,
         children: <Widget>[
-          /// 'BARIS' SELAMAT DATANG
-          ListTile(
-            dense: true,
-            contentPadding: EdgeInsets.fromLTRB(15, 0, 15, 7),
-            tileColor: Colors.lightBlueAccent[200],
-            title: Text('Selamat datang di danafix',
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.white, fontSize: 22)),
-          ),
+          /// layout atas dan tengah
           Container(
-            padding: EdgeInsets.fromLTRB(15, 0, 15, 15),
-            color: Colors.lightBlueAccent[200],
-            child: Text(
-              'Izinkan kami menggunakan beberapa informasi pribadi dan non-pribadi '
-              'Anda untuk memproses aplikasi Anda dan menyediakan layanan kami',
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.white, fontSize: 12),
+            height: 400,
+            child: ListView(
+              children: <Widget>[
+                /// 'BARIS' SELAMAT DATANG
+                ListTile(
+                  dense: true,
+                  contentPadding: EdgeInsets.fromLTRB(15, 0, 15, 7),
+                  tileColor: Colors.lightBlueAccent[200],
+                  title: Text('Selamat datang di danafix',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white, fontSize: 22)),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(15, 0, 15, 15),
+                  color: Colors.lightBlueAccent[200],
+                  child: Text(
+                    'Izinkan kami menggunakan beberapa informasi pribadi dan non-pribadi '
+                        'Anda untuk memproses aplikasi Anda dan menyediakan layanan kami',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white, fontSize: 12),
+                  ),
+                ),
+                Row(children: <Widget>[
+                  Expanded(
+                    child: Row(children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.fromLTRB(15, 0, 15, 15),
+                        child: Icon(Icons.call),
+                      ),
+                      Expanded(
+                        child: Container(
+                          /// IF ALL COMPONENTS ARE DONE AND THE LAYOUT CHANGED, TRY CHANGE THE 42
+                            padding: EdgeInsets.fromLTRB(0, 42, 15, 15),
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text(
+                                    'Informasi telepon',
+                                    textAlign: TextAlign.justify,
+                                    style: TextStyle(color: Colors.black),
+                                  ),
+                                  Text(
+                                    'Kami mencatat dan menggunakan informasi ponsel Anda, seperti IMEI, informasi perangkat lunak, dll. '
+                                        'Untuk mendapatkan data statistik dan menganalisis pengiriman layanan Aplikasi dan untuk '
+                                        'meningkatkan kinerja aplikasi di wilayah Anda',
+                                    textAlign: TextAlign.justify,
+                                    style: TextStyle(color: Colors.grey[600], fontSize: 12),
+                                  ),
+                                ]
+                            )
+                        ),
+                      )
+                    ]),
+                  ),
+                ]),
+                Row(children: <Widget>[
+                  Expanded(
+                    child: Row(children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.fromLTRB(15, 0, 15, 15),
+                        child: Icon(Icons.location_pin),
+                      ),
+                      Expanded(
+                        child: Container(
+                            padding: EdgeInsets.fromLTRB(0, 15, 15, 15),
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text(
+                                    'Posisi geografis',
+                                    textAlign: TextAlign.justify,
+                                    style: TextStyle(color: Colors.black),
+                                  ),
+                                  Text(
+                                    'Kami mencatat dan menggunakan posisi Anda untuk meningkatkan kinerja aplikasi di wilayah Anda',
+                                    textAlign: TextAlign.justify,
+                                    style: TextStyle(color: Colors.grey[600], fontSize: 12),
+                                  ),
+                                ]
+                            )
+                        ),
+                      )
+                    ]),
+                  ),
+                ]),
+                Row(children: <Widget>[
+                  Expanded(
+                    child: Row(children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.fromLTRB(15, 0, 15, 15),
+                        child: Icon(Icons.camera_alt),
+                      ),
+                      Expanded(
+                        child: Container(
+                            padding: EdgeInsets.fromLTRB(0, 15, 15, 15),
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text(
+                                    'Kamera',
+                                    textAlign: TextAlign.justify,
+                                    style: TextStyle(color: Colors.black),
+                                  ),
+                                  Text(
+                                    'Kami menggunakan kamera Anda untuk mengambil foto Anda dan mengunggahnya ke aplikasi Anda',
+                                    textAlign: TextAlign.justify,
+                                    style: TextStyle(color: Colors.grey[600], fontSize: 12),
+                                  ),
+                                ]
+                            )
+                        ),
+                      )
+                    ]),
+                  ),
+                ]),
+
+                /// 'baris' bawah
+                /*Row(
+            children: <Widget>[
+              Text('data'),
+              Spacer(),
+              Text('data'),
+            ]
+          )*/
+              ]
             ),
           ),
-          Row(children: <Widget>[
-            Expanded(
-              child: Row(children: <Widget>[
+
+          /// layout terima
+          Container(
+            height: 200,
+            child: ListView(
+              children: <Widget>[
                 Container(
-                  padding: EdgeInsets.fromLTRB(15, 0, 15, 15),
-                  child: Icon(Icons.call),
-                ),
-                Expanded(
-                  child: Container(
-                    /// IF ALL COMPONENTS ARE DONE TRY CHANGE THE 42
-                      padding: EdgeInsets.fromLTRB(0, 42, 15, 15),
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              'Informasi telepon',
-                              textAlign: TextAlign.justify,
-                              style: TextStyle(color: Colors.black),
+                  height: 200,
+                  color: Colors.yellowAccent,
+                  child: Stack(
+                    children: <Widget>[
+                      Align(
+                          alignment: FractionalOffset.center,
+                          child: Text(
+                            "₹ 1000",
+                          )
+                      ),
+                      Positioned(
+                          bottom: 0,
+                          child: RaisedButton(
+                            color: Colors.brown,
+                            onPressed: null,
+                            child: new Text(
+                                'OK'
                             ),
-                            Text(
-                              'Kami mencatat dan menggunakan informasi ponsel Anda, seperti IMEI, informasi perangkat lunak, dll. '
-                                  'Untuk mendapatkan data statistik dan menganalisis pengiriman layanan Aplikasi dan untuk '
-                                  'meningkatkan kinerja aplikasi di wilayah Anda',
-                              textAlign: TextAlign.justify,
-                              style: TextStyle(color: Colors.grey[600], fontSize: 12),
-                            ),
-                          ]
+                          )
                       )
+                    ],
                   ),
                 )
-              ]),
+              ]
             ),
-          ]),
-          Row(children: <Widget>[
-            Expanded(
-              child: Row(children: <Widget>[
-                Container(
-                  padding: EdgeInsets.fromLTRB(15, 0, 15, 15),
-                  child: Icon(Icons.pin_drop),
-                ),
-                Expanded(
-                  child: Container(
-                      padding: EdgeInsets.fromLTRB(0, 15, 15, 15),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              'Posisi geografis',
-                              textAlign: TextAlign.justify,
-                              style: TextStyle(color: Colors.black),
-                            ),
-                            Text(
-                              'Kami mencatat dan menggunakan posisi Anda untuk meningkatkan kinerja aplikasi di wilayah Anda',
-                              textAlign: TextAlign.justify,
-                              style: TextStyle(color: Colors.grey[600], fontSize: 12),
-                            ),
-                          ]
-                      )
-                  ),
-                )
-              ]),
-            ),
-          ])
+          )
         ],
+
       ),
     );
   }
