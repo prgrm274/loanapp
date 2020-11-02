b4cd1a89a8e45bcfbe061c4cb136614350c51022
prgrm274 <programmer270487@gmail.com>
2020-11-02 13:03:23 +0700

    peminjaman

--- lib/apps/home/home.dart
+++ lib/apps/home/home.dart
@@ -44,22 +44,74 @@ class _HomeState extends State<Home> {
               ),
             ]),
             Column(children: <Widget>[
-              Container(
+              Container(/// KONTAINER TEXT LIST
                 margin: EdgeInsets.fromLTRB(8, 40, 8, 0),
                 height: 200,
                 width: MediaQuery.of(context).size.width * 1,
                 decoration: BoxDecoration(
                   color: Colors.white,
-                  border: Border.all(width: 3, color: Colors.transparent),
+                  border: Border.all(width: 1, color: Colors.red),
                   borderRadius: BorderRadius.all(Radius.circular(4)),
                 ),
                 child: ListView(
                   children: <Widget>[
-                    Text(
-                      'Anda terima',
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      color: Colors.blue,
+                      child: Row(
+                        children: <Widget>[
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: Text(
+                              'Anda terima',
+                            ),
+                          ),
+                          Image(
+                            height: 24,
+                            width: 24,
+                            image: AssetImage('lib/assets/info_48.png'),
+                          ),
+                          Align(
+                            /// BERI JARAK ANTARA anda terima DAN rp 1,5jt
+                            alignment: Alignment.topCenter,
+                            child: Container(
+                              width: MediaQuery.of(context).size.width * 0.5,
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment.topRight,
+                            child: Text(
+                              'Rp 1.500.000',
+                            ),
+                          ),
+                        ]
+                      )
                     ),
-                    Text(
-                      'Anda terima',
+                    Container(
+                        padding: EdgeInsets.all(10),
+                        color: Colors.yellow,
+                        child: Row(
+                            children: <Widget>[
+                              Align(
+                                alignment: Alignment.topLeft,
+                                child: Text(
+                                  'Anda mengembalikan',
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.topCenter,
+                                child: Container(
+                                  width: MediaQuery.of(context).size.width * 0.355,
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.topRight,
+                                child: Text(
+                                  'Rp 2.625.000',
+                                ),
+                              ),
+                            ]
+                        )
                     ),
                   ]
                 )
